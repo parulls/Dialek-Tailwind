603cdc529535e644eb2e96dd1e4b7436a4db4f4e
rielyta <desi@students.usu.ac.id>
2024-12-01 13:53:12 +0700

    loginku rusak ntah knp, jdi ak perbaiki

--- src/php/login.php
+++ src/php/login.php
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <div class="mb-4">
                         <label class="block text-custom2 text-lg font-semibold mb-2" for="password">Kata Sandi</label>
                         <div class="input-container flex items-center bg-bg-form border border-gray-300 rounded-lg">
-                            <input type="password" name="password" id="password" placeholder="*********" class="w-full text-sm bg-bg-form px-3 py-2 border-none rounded-lg text-form focus:outline-none" aria-label="Password" />
+                            <input type="password" name="password" id="password" placeholder="*" class="w-full text-sm bg-bg-form px-3 py-2 border-none rounded-lg text-form focus:outline-none" aria-label="Password" />
                             <ion-icon name="lock-closed" class="pr-4 items-center"></ion-icon>
                         </div>
                     </div>
@@ -121,7 +121,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         </div>
                     </div>
                     <div class="text-center mt-4">
-                        <p class="text-black">Belum Punya Akun? <a href="./signup.php" class="text-custom1 font-bold hover:underline">Daftar</a></p>
+                        <p class="text-black">Belum Punya Akun? <a href="./SignUp.php" class="text-custom1 font-bold hover:underline">Daftar</a></p>
                     </div>
                 </form>
             </div>
@@ -129,10 +129,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </section>
 
     <script type="module">
-        document.getElementById("signup-button").addEventListener("click", () => {
-        window.location.href = "signup.php"; // Arahkan ke signup.php
-        });
-
         AOS.init();
         import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
         import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
